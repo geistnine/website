2e31bad9eb313b31e87814ff2a1a78837fa3779e
Steve <sgeiger5516@gmail.com>
2023-03-06 18:21:55 -0500

    added jsx skeleton for main.tsx

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,7 +1,21 @@
 export default function Main(){
     return (
-        <div className="" style={{animation: "0.75s ease-out 0s 0.75 appear"}}>
+        <div className="flex justify-center items-center" style={{animation: "0.75s ease-out 0s 0.75 appear"}}>
             Placeholder
+            <div id="biocard">
+                <div id="headshot">
+                    Image goes here.
+                </div>
+                <div id="biography">
+                    Biography goes here.
+                </div>
+            </div>
+            <div id="techcard">
+                Technologies go here.
+            </div>
+            <div id="portfolio">
+                Projects go here.
+            </div>
         </div>
     )
 }
