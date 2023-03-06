993c442cd72078e8361a7c05840ab57a7fb7f9cd
Steve <sgeiger5516@gmail.com>
2023-03-06 18:11:21 -0500

    added fadein effect on main page render

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,6 @@
 export default function Main(){
     return (
-        <div>
+        <div className="" style={{animation: "0.75s ease-out 0s 0.75 appear"}}>
             Placeholder
         </div>
     )
